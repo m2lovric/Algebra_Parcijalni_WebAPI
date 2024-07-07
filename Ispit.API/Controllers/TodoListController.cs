8517d0d79c21c458df3c6fdd819cc05b95d3f158
m2lovric <m2lovric@gmail.com>
2024-07-07 21:01:07 +0200

    Add project files.

---
+++ Ispit.API/Controllers/TodoListController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Ispit.API.Models;
+
+namespace Ispit.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoListController : ControllerBase
+    {
+        private readonly TodoListContext _context;
+
+        public TodoListController(TodoListContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TodoList
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoList>>> GetTodoList()
+        {
+            return await _context.TodoList.ToListAsync();
+        }
+
+        // GET: api/TodoList/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoList>> GetTodoList(int id)
+        {
+            var todoList = await _context.TodoList.FindAsync(id);
+
+            if (todoList == null)
+            {
+                return NotFound();
+            }
+
+            return todoList;
+        }
+
+        // PUT: api/TodoList/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoList(int id, TodoList todoList)
+        {
+            if (id != todoList.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(todoList).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoListExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TodoList
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TodoList>> PostTodoList(TodoList todoList)
+        {
+            _context.TodoList.Add(todoList);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTodoList", new { id = todoList.Id }, todoList);
+        }
+
+        // DELETE: api/TodoList/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoList(int id)
+        {
+            var todoList = await _context.TodoList.FindAsync(id);
+            if (todoList == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoList.Remove(todoList);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TodoListExists(int id)
+        {
+            return _context.TodoList.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Ispit.API/Models/TodoListContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ispit.API.Models
+{
+    public class TodoListContext : DbContext
+    {
+        public TodoListContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<TodoList> TodoList { get; set; }
+    }
+}
